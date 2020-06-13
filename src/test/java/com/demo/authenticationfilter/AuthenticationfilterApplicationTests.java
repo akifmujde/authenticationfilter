5949d028713131fcbc60c0803828acd02ce208bc
Mehmet akif Müjde <mehmetakifmujde@Mehmet-MacBook-Pro.local>
2020-06-13 22:46:57 +0300

    default project

---
+++ src/test/java/com/demo/authenticationfilter/AuthenticationfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.authenticationfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
