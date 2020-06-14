60b7f0888dd2eb3ccf2236abdb1e559c51196fe7
Mehmet akif Müjde <mehmetakifmujde@Mehmet-MacBook-Pro.local>
2020-06-14 14:10:21 +0300

    Configuration is edited.

--- src/main/java/com/demo/authenticationfilter/configuration/database/MySqlDatabaseConfig.java
+++ src/main/java/com/demo/authenticationfilter/configuration/database/MySqlDatabaseConfig.java
@@ -1,4 +1,4 @@
-package com.demo.authenticationfilter.configuration;
+package com.demo.authenticationfilter.configuration.database;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
